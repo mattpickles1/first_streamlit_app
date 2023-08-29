9048ae3de166b51fd5b671efd5e21cdd0173b571
mattpickles1 <117274125+mattpickles1@users.noreply.github.com>
2023-08-29 17:28:41 +0200

    Update streamlit_app.py
    Updated Title & Header's

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free-range Egg')
