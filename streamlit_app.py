2117299c3563143a660093ce6ec72dca7be2bd64
mattpickles1 <117274125+mattpickles1@users.noreply.github.com>
2023-08-29 17:35:11 +0200

    Update streamlit_app.py
    Adding apostrofe in title

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-streamlit.title('My Moms New Healthy Diner')
+streamlit.title('My Mom´s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
