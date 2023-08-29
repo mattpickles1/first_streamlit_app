6b9004f026adb173134c648fa08673484b0aa762
mattpickles1 <117274125+mattpickles1@users.noreply.github.com>
2023-08-29 17:12:16 +0200

    Update streamlit_app.py
    Fixed reserved character

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 
 import streamlit
 
-streamlit.title('My Parent's New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
