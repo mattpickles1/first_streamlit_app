9542d65d2cc9d166b9ffa293d1d0a8ac54ce8dfa
mattpickles1 <117274125+mattpickles1@users.noreply.github.com>
2023-08-30 15:10:28 +0200

    Update streamlit_app.py
    Added normalized json version of data

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,15 @@ streamlit.dataframe(fruits_to_show)
 #new section to show Fruity Vice API data
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+#take the json version of the reponse and normalize it
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_reponse.json())
+
+#output the screen as a table
+
+streamlit.dataframe(fruityvice_normalized)
+
 
 
