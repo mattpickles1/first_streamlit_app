ca4490bc320985ab130d9071ce07d4d78b566df0
mattpickles1 <117274125+mattpickles1@users.noreply.github.com>
2023-08-30 15:15:34 +0200

    Update streamlit_app.py
    Adding fruit choice functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,10 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to show Fruity Vice API data
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of the reponse and normalize it
 
