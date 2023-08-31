ca1483edcbebbea4384a1c2e1ce0cbba2a2952b1
mattpickles1 <117274125+mattpickles1@users.noreply.github.com>
2023-08-31 10:46:03 +0200

    Update streamlit_app.py
    Adding new fruit_list_load functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,18 +29,21 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to show Fruity Vice API data
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get mor information.")
+else
+
+#take the json version of the reponse and normalize it and output the screen as a table
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())   
+streamlit.dataframe(fruityvice_normalized) 
 
-#take the json version of the reponse and normalize it
+except URLerror as e:
+  streamlit.error()
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#output the screen as a table
-
-streamlit.dataframe(fruityvice_normalized)
 
 #breakpoint for debugging
 streamlit.stop()
