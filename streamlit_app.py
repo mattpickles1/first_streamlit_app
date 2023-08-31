8bb73a7f58e0bc352ebb3602434ea1a7132be302
mattpickles1 <117274125+mattpickles1@users.noreply.github.com>
2023-08-31 12:17:25 +0200

    Update streamlit_app.py
    Fixed code indents

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,12 @@ def insert_row_snowflake(new_fruit):
 try:        
     add_my_fruit = streamlit.text_input('What fruit would you like to add to the list?')
     if not add_my_fruit:
-    streamlit.error("Please choose a fruit to add.")
-  else:
-    if streamlit.button('Add a fruit to the list'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
+        streamlit.error("Please choose a fruit to add.")
+    else:
+        if streamlit.button('Add a fruit to the list'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        back_from_function = insert_row_snowflake(add_my_fruit)
+        streamlit.text(back_from_function)
 
 #breakpoint for code consistency
 streamlit.stop()
